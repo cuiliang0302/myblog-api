98c2148077e101bae95a8312535485aae8d57468
Liang.Cui <cuiliang0302@qq.com>
2024-12-07 21:54:42 +0800

    更新启动脚本，解决证书检测问题

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "正在启动后端API服务……"
 nohup gunicorn DRF.asgi:application -k uvicorn.workers.UvicornWorker -c gunicorn_conf.py > logs/django.log 2>&1 &
 sleep 5
 echo "正在启动定时任务……"
-nohup python3.11 manage.py crontab > logs/crontab.log 2>&1 &
+nohup python manage.py crontab > logs/crontab.log 2>&1 &
 sleep 5
 echo "服务启动成功！"
 tail -f logs/django.log
