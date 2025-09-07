f515fce5e84f4da72001f597501e9d0c5be46700
Liang.Cui <cuiliang0302@qq.com>
2025-09-07 09:50:57 +0800

    更新定时任务

--- public/management/commands/crontab.py
+++ public/management/commands/crontab.py
@@ -114,7 +114,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             sitemap_job,
-            trigger=CronTrigger(hour="06", minute="00"),
+            trigger=CronTrigger(hour="07", minute="00"),
             id="sitemap_job",  # The `id` assigned to each job MUST be unique
             max_instances=5,
             replace_existing=True,
@@ -135,7 +135,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             async_note_job,
-            trigger=CronTrigger(hour="1-5", minute="00"),
+            trigger=CronTrigger(hour="2-6", minute="00"),
             # trigger=CronTrigger(second='*/10'),  # Every 10 seconds
             id="async_note_job",  # The `id` assigned to each job MUST be unique
             max_instances=5,
